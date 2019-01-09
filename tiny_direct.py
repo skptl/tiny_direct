ef60f806c7de126c1f37930c1d1a21dfa6786f55
semal259 <semalpatel2596@gmail.com>
2019-01-09 12:38:37 -0800

    fix redirects(works now)

--- tiny_direct.py
+++ tiny_direct.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,8 +8,10 @@ _semal = _github + 'semal259/'
 _charles = _github + 'acharles7/'
 
 SEMAL = {
-    'root': _semal,
-    'a': _semal + 'SnapNotes'
+    'root' : _semal,
+    'a' : _semal + 'SnapNotes',
+    'b' : _semal + 'vagabond',
+    'c' : _semal + 'Twitterty'
 }
 
 CHARLES = {
@@ -16,24 +19,25 @@ CHARLES = {
     'a': _charles + 'Twitterty'
 }
 
-@app.route('/test/awesome')
-def test():
-    return "Hi, teddy!"
-
 @app.route('/<path>')
 def root(path):
-    if len(path) > 1:
-        if path[0] == 'S' or 's':
+    if path[0] == 'S' or path[0] == 's':
+        print("semal")
+        try:
             if path[1] in SEMAL:
                 return redirect(SEMAL[path[1]], code=302)
-            else:
-                return redirect(SEMAL['root'], code=302)
-        elif path[0] == 'C' or 'c':
+        except IndexError:
+            print("semal index")
+            return redirect(SEMAL['root'], code=302)
+    elif path[0] == 'C' or path[0] == 'c':
+        try:
+            print("charles")
             if path[1] in CHARLES:
                 return redirect(CHARLES[path[1]], code=302)
-            else:
-                return redirect(CHARLES['root'], code=302)
-    return redirect(_github, code=302)
+        except IndexError:
+            return redirect(CHARLES['root'], code=302)
+    else:
+        return redirect(_github, code=302)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
